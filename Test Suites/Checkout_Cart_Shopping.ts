862989b3b3155822c3517ee926f98111881fc446
ferdin permana <ferdin.permana@gmail.com>
2026-05-30 20:45:46 +0700

    ci: fix architecture layout and sync underscore test suite

--- Test Suites/Checkout_Cart_Shopping.ts
+++ Test Suites/Checkout_Cart_Shopping.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description></description>
-   <name>Checkout Cart Shooping</name>
+   <name>Checkout_Cart_Shopping</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
